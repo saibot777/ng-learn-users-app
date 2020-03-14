030a1fa5562cd72cbf1a42b6e738827100df5e0d
saibot777 <stefantrajkovic777@gmail.com>
2020-03-14 22:53:31 +0100

    users store - actions impl

---
+++ src/app/users/store/users.action.ts
@@ -0,0 +1,20 @@
+import { createAction, props } from "@ngrx/store";
+import { User } from "../user.model";
+
+export const loadUsers = createAction("[Users] Load All Users");
+
+export const usersLoaded = createAction(
+  "[Users] Users Loaded Successfully",
+  props<{ users: User[] }>()
+);
+
+export const createUser = createAction(
+  "[Users] Create User",
+  props<{ user: User }>()
+);
+
+export const courseActionTypes = {
+  loadUsers,
+  usersLoaded,
+  createUser
+};
