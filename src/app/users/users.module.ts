e2f26d07a6ec11fda5083388f7e2d669518daff8
saibot777 <stefantrajkovic777@gmail.com>
2020-03-14 23:37:10 +0100

    proxy conf impl

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -1,10 +1,20 @@
+import { FormsModule } from "@angular/forms";
 import { NgModule } from "@angular/core";
+import { StoreModule } from "@ngrx/store";
+import { EffectsModule } from "@ngrx/effects";
 import { CommonModule } from "@angular/common";
 import { UsersService } from "./users.service";
+import { userReducer } from "./store/users.reducer";
+import { UsersEffects } from "./store/users.effects";
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    StoreModule.forFeature("users", userReducer),
+    EffectsModule.forFeature([UsersEffects])
+  ],
   providers: [UsersService]
 })
 export class UsersModule {}
